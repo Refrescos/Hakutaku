7657df34cbbb117f5c7ac480511ba6c07917c331
rodrigocampos3 <rodrigo.rodrigues@sou.inteli.edu.br>
2024-09-01 10:30:57 -0300

    tabelas

--- sources/dashboard/app/(raw)/Home/page.tsx
+++ sources/dashboard/app/(raw)/Home/page.tsx
@@ -11,13 +11,24 @@ import { HiOutlineClipboardList } from "react-icons/hi";
 import { CiSettings } from "react-icons/ci";import { FiPieChart, FiHome } from "react-icons/fi";
 import { GoGitPullRequest } from "react-icons/go";
 import { FaCalendar } from "react-icons/fa";
+import BarChart from '@/components/BarChart';
 import { IoMdAlert } from "react-icons/io";
 import BasicBars from '@/components/BarChart';
 const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
 const xLabels = ['Page A', 'Page B', 'Page C', 'Page D', 'Page E', 'Page F', 'Page G'];
 
+const data = [12, 19, 3, 5, 2, 3];
+  const labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'];
+  const barColor = ''; // Cor das barras
+
+
 const Home: React.FC = () => {
+
+    const data = [5, 10, 15, 20, 25, 30];
+    const labels = ['Categoria 1', 'Categoria 2', 'Categoria 5', 'Categoria 6'];
+    const barColor = '#ED8936'; // Cor das barras
+    const barThickness = 15 ;
     return (
         <section className="flex flex-col items-center min-w-screen justify-center h-full bg-[#EDE9E2]">
             <div className="flex flex-row min-w-full text-center min-h-full">
@@ -115,12 +126,60 @@ const Home: React.FC = () => {
                         </div>
 
                     </div>
-                    <div className='w-full flex flex-col '>
-                        <div className='w-1/2'>
-                  
-        
+                    <div className='w-full flex flex-row px-20'>
+                        <div className='w-1/3 h-[300px]'>
+                        <BarChart data={data} labels={labels} barColor={barColor} barThickness={barThickness} />
                         </div>
-                        <div className='w-1/2'>
+                        <div className='w-2/3'>
+                        <div className='w-full p-4 '>
+                            <div className="bg-white rounded-lg p-4 shadow-md flex flex-col">
+                                <div className='flex flex-row justify-between border-b-1 py-1'>
+                                    <div className='flex flex-col'>
+                                        <h2 className="font-semibold text-[#A0AEC0]">PROBLEM SOLVING</h2>
+                                        <h2 className="font-semibold">Índice de resolução de problema por produto</h2>
+                                    </div>
+                                    <button className='bg-black p-2 text-white rounded-xl'> Text</button>
+                                </div>
+
+                                {/* Tabela abaixo dos textos */}
+                                <table className="mt-4 w-full">
+                                    <thead>
+                                        <tr className="bg-white">
+                                            <th className="p-2 text-left">PRODUTO</th>
+                                            <th className="p-2 text-left">REQ. PARA RESOLUÇÃO</th>
+                                                <th className="p-2 text-left">Progresso</th>
+                                                <th className="p-2 text-left">Progresso</th>
+
+                                                
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td className="p-2 border-b">Cartão</td>
+                                                <td className="p-2 border-b">1</td>
+                                                <td className="p-2 border-b">Cartão</td>
+
+                                            <td className="p-2 border-b">120</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="p-2 border-b">API</td>
+                                                <td className="p-2 border-b">Dados 5</td>
+                                                <td className="p-2 border-b">Cartão</td>
+
+                                            <td className="p-2 border-b">100</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="p-2 border-b">PIX</td>
+                                                <td className="p-2 border-b">Dados 8</td>
+                                                <td className="p-2 border-b">Cartão</td>
+
+                                            <td className="p-2 border-b">90</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
 
                         </div>
 
