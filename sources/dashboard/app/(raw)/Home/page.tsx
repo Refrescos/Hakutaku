aa2e89fd749a8b233d7c1c74e23e1432c71f9935
rodrigocampos3 <rodrigo.rodrigues@sou.inteli.edu.br>
2024-09-01 00:08:29 -0300

    inicio page dashboard

--- sources/dashboard/app/(raw)/Home/page.tsx
+++ sources/dashboard/app/(raw)/Home/page.tsx
@@ -1,34 +1,53 @@
 import { Link } from '@nextui-org/link';
-import { Snippet } from '@nextui-org/snippet';
-import { Code } from '@nextui-org/code';
-import { button as buttonStyles } from '@nextui-org/theme';
 import Image from 'next/image';
-import { Button } from '@nextui-org/button';
-import starkbank from "public/starkbank.png";
-import IconStark from '@/components/icons/IconStark';
+import { FiPieChart } from "react-icons/fi";
+import { FiHome } from "react-icons/fi";
+
+
+import { FaHome, FaUser, FaCog, FaEnvelope } from 'react-icons/fa'; // Importando ícones
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center min-w-screen justify-center h-full bg-[#EDE9E2]">
-			<div className="flex flex-row min-w-full text-center min-h-full">
-				{/* Sidebar */}
-				<div className="w-1/10 bg-[#E25F2B] p-4 flex flex-col"> {/* Ajuste a cor e o preenchimento conforme necessário */}
-					{/* Conteúdo da Sidebar */}
-					<h2 className="text-lg font-bold">Sidebar</h2>
-					<Image src="/Vector.png" alt='logo' width={150} height={150}></Image>
-					<ul>
-						<li><Link href="/">Link 1</Link></li>
-						<li><Link href="/">Link 2</Link></li>
-						<li><Link href="/">Link 3</Link></li>
+    return (
+        <section className="flex flex-col items-center min-w-screen justify-center h-full bg-[#EDE9E2]">
+            <div className="flex flex-row min-w-full text-center min-h-full">
+                {/* Sidebar */}
+                <div className="w-1/8 bg-[#E25F2B] p-4 flex flex-col items-center"> {/* Ajuste a largura conforme necessário */}
+                    {/* Conteúdo da Sidebar */}
+                    <Image src="/hakutaku.png" alt='logo' width={70} height={70} />
+                    <ul className="mt-4 space-y-10"> {/* Espaçamento entre os ícones */}
+                        <li className="flex flex-col items-center">
+                            <Link href="/" className="flex items-center">
+                                <FiPieChart className="text-2xl mr-2" />
+                                
+                            </Link>
+                        </li>
+                        <li className="flex flex-col items-center">
+                            <Link href="/" className="flex items-center">
+                                <FiHome className="text-2xl mr-2" />
+                                
+                            </Link>
+                        </li>
+                        <li className="flex flex-col items-center">
+                            <Link href="/" className="flex items-center">
+                                <FiHome className="text-2xl mr-2" />
+                            </Link>
+                        </li>
+                        <li className="flex flex-col items-center">
+                            <Link href="/" className="flex items-center">
+                                <FaEnvelope className="text-2xl mr-2" />
+                                
+                            </Link>
+                        </li>
 					</ul>
-				</div>
+		
+                </div>
 
-				{/* Conteúdo principal */}
-				<div className="flex-grow bg-white p-4">
-					{/* Aqui você pode adicionar o conteúdo principal da sua página */}
-					<h1 className="text-2xl">Conteúdo Principal</h1>
-				</div>
-			</div>
-		</section>
-	);
+                {/* Conteúdo principal */}
+                <div className="flex-grow bg-white p-4">
+                    {/* Aqui você pode adicionar o conteúdo principal da sua página */}
+                    <h1 className="text-2xl">Conteúdo Principal</h1>
+                </div>
+            </div>
+        </section>
+    );
 }
